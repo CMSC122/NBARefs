3c0b01236a5fb4c0ec01041df51d71d042a40cf1
fernandodestefanis <destefanis@uchicago.edu>
2017-03-04 14:01:35 -0600

    Add files via upload

---
+++ create-db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE referees
+  (game_code varchar(12),
+   referee_name varchar(50));
+
+.import Output.csv referees
+
+
+CREATE TABLE calls
+  (game_name varchar(50),
+   time varchar(10),
+   period varchar(5),
+   call_type varchar(50),
+   committing_player varchar(50),
+   disadvantaged_player varchar(50),
+   call_accuracy varchar(10),
+   comment varchar(300)
+   game_code varchar(12));
+
+.import data_dict.csv calls
